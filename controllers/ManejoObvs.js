e6f9d7b3e661fc44562f753d0c9eb94f428b000a
dsandoval-a <dsandovala@ucenfotec.ac.cr>
2023-11-25 23:13:09 -0600

    observer

--- controllers/ManejoObvs.js
+++ controllers/ManejoObvs.js
@@ -4,3 +4,15 @@ carrerasEmitter.on('carreraUpdate', (codigoCarrera) => {
     // Aquí puedes realizar las acciones necesarias cuando se actualiza una carrera
     console.log(`Carrera actualizada: ${codigoCarrera}`);
 });
+
+matriculaEmitter.on('matriculaUpdate', (cedula) => {
+    // Aquí puedes realizar las acciones necesarias cuando se actualiza una carrera
+    console.log(`Curso actualizado de matricula: ${cedula}`);
+});
+
+
+
+matriculaBorraEmitter.on('matriculaBorraUpdate', (cedula) => {
+    // Aquí puedes realizar las acciones necesarias cuando se actualiza una carrera
+    console.log(`Curso Eliminado de matricula: ${cedula}`);
+});

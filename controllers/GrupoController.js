4aaf420e4833350defac25a781e118312a59edae
Larkorin <michael162003@gmail.com>
2023-12-05 17:55:00 -0600

    Fixed Grupo Controller Routes

--- controllers/GrupoController.js
+++ controllers/GrupoController.js
@@ -3,7 +3,7 @@
 const Grupo = require("../models/Grupo.js");
 
 module.exports = function (app) {
-  //http://localhost:3000/grupo/profesor/:cedula/ciclo-activo/1 a la Programación
+  //http://localhost:3000/grupo/profesor/003-123456-0002C/ciclo-activo/2
   app.get(
     "/grupo/profesor/:cedulaProfesor/ciclo-activo/:cicloActivo",
     async (req, res) => {
@@ -14,7 +14,7 @@ module.exports = function (app) {
         const grupos = await Grupo.find({
           profesor: cedulaProfesor,
           ciclo: cicloActivo,
-        }).toArray();
+        });
 
         if (grupos.length === 0) {
           res
@@ -33,7 +33,7 @@ module.exports = function (app) {
     }
   );
 
-  //http://localhost:3000/grupo/1/cedula-estudiante/:cedula/actualizar-notas/notas/85 a la Programación
+  //http://localhost:3000/registrar-notas/a316247a8188a594ff826d41
   app.post("/registrar-notas/:grupoId", async (req, res) => {
     const grupoId = req.params.grupoId;
     const notas = req.body.notas; // Espera un objeto con las notas
@@ -65,6 +65,7 @@ module.exports = function (app) {
   });
 
   //Encontrar Grupos por Curso
+  //http://localhost:3000/grupo/1
   app.get("/grupo/:cursoSeleccionado", async (req, res) => {
     const cursoSeleccionado = parseInt(req.params.cursoSeleccionado);
 
@@ -86,55 +87,72 @@ module.exports = function (app) {
   });
 
   //Modificar asistencia
+  //http://localhost:3000/grupo/1/cedulaAlumno/007-123456-0006G/asistencia/presente
   app.put(
     "/grupo/:cursoSeleccionado/cedulaAlumno/:cedulaAlumno/asistencia/:tipoAsistencia",
     async (req, res) => {
       const cursoSeleccionado = parseInt(req.params.cursoSeleccionado);
       const cedulaAlumno = req.params.cedulaAlumno;
       const tipoAsistencia = req.params.tipoAsistencia;
-
+  
       try {
         const grupos = await Grupo.find({ curso: cursoSeleccionado });
-
+        console.log(grupos);
         if (grupos.length === 0) {
-          res
-            .status(404)
-            .send({
-              mensaje: `No hay grupos disponibles para el curso con código ${cursoSeleccionado}`,
-            });
+          res.status(404).send({
+            mensaje: `No hay grupos disponibles para el curso con código ${cursoSeleccionado}`,
+          });
         } else {
-          const estudiante = grupo.estudiantes.find(est => est.cedula === cedulaAlumno) || {};
+          console.log("xxxxxxxxxxxxxx");
           
-          if (!estudiante.cedula) {
-            return res.status(404).send('Estudiante no encontrado en este grupo');
+          // Iterar sobre los grupos y buscar el estudiante en cada grupo
+          let estudianteEncontrado = false;
+          for (const grupo of grupos) {
+            const estudiante = grupo.estudiantes.find(est => est.cedula === cedulaAlumno);
+            console.log("-------------");
+            console.log(estudiante);
+  
+            if (estudiante) {
+              estudianteEncontrado = true;
+  
+              switch (tipoAsistencia) {
+                case 'ausencias':
+                  estudiante.asistencia_ausencias += 1;
+                  break;
+                case 'ausencias_justificadas':
+                  estudiante.asistencia_ausencias_justificadas += 1;
+                  break;
+                case 'presente':
+                  estudiante.asistencia_presente += 1;
+                  break;
+                case 'tardias':
+                  estudiante.asistencia_tardias += 1;
+                  break;
+                default:
+                  return res.status(400).send('Tipo de asistencia no válido');
+              }
+  
+              await grupo.save();
+  
+              break;
+            }
           }
-
-          switch (tipoAsistencia) {
-            case 'ausencias':
-              estudiante.asistencia_ausencias += 1;
-              break;
-            case 'ausencias_justificadas':
-              estudiante.asistencia_ausencias_justificadas += 1;
-              break;
-            case 'presente':
-              estudiante.asistencia_presente += 1;
-              break;
-            case 'tardias':
-              estudiante.asistencia_tardias += 1;
-              break;
-            default:
-              return res.status(400).send('Tipo de asistencia no válido');
+  
+          if (!estudianteEncontrado) {
+            return res.status(404).send('Estudiante no encontrado en ningún grupo');
           }
-        
+  
           return res.send('Asistencia actualizada correctamente');
-          
         }
       } catch (err) {
         res.status(500).send(err);
+        console.log(err);
       }
-    });
+    }
+  );
+  
 
-    //GET http://localhost:3000/grupo/12345/estudiantes
+    //http://localhost:3000/grupo/a316247a8188a594ff826d41/estudiantes
     app.get('/grupo/:grupoId/estudiantes', async (req, res) => {
       try {
           const grupoId = req.params.grupoId;
@@ -153,6 +171,7 @@ module.exports = function (app) {
   });
 
   // Ruta para obtener las asistencias de los estudiantes en un grupo
+  //http://localhost:3000/asistencias/a316247a8188a594ff826d41
   app.get('/asistencias/:grupoId', async (req, res) => {
     const grupoId = req.params.grupoId;
 
@@ -173,7 +192,7 @@ module.exports = function (app) {
         });
         res.json(asistenciasEstudiantes);
       }
-    } catch (error) {
+    } catch (err) {
       res.status(500).send(err);
     }
   });

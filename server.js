854d990a495214624373c573e042926562ad546f
Larkorin <michael162003@gmail.com>
2023-12-06 20:51:11 -0600

    Fixed cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./db');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
